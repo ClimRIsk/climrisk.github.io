25d460d958f2c952a4d892d6dce446a7270c41d3
ClimRIsk <ClimRIsk@users.noreply.github.com>
2026-07-16 16:24:42 +0000

    deploy: 7c205180b99ae58b2338d708b0cfcad6fc77e906

--- _next/static/chunks/app/contact/page-e5c17421969a19d8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[327,668,20,722,950,214,641,753,580],{1869:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=1869)}),_N_E=n.O()}]);
